5c6b6de356e20dd86aaf2cc4943da3687fa72b9c
dharmesh-tukadiya <dharmeshntukadiya@gmail.com>
2025-07-01 19:33:52 +0000

    Made Material request and purchase order request from the grid.

--- supplier_management/supplier_management/report/items_needed/items_needed.js
+++ supplier_management/supplier_management/report/items_needed/items_needed.js
@@ -63,4 +63,157 @@ frappe.query_reports["Items Needed"] = {
 			width: 90,
 		},
 	],
+
+	formatter(value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		
+		// Highlight negative projected qty in red
+		if (column.fieldname === "projected_qty" && data.projected_qty < 0) {
+			value = `<span style="color:red; font-weight: bold;">${value}</span>`;
+		}
+		
+		return value;
+	},
+
+	get_datatable_options(options) {
+		return Object.assign(options, {
+			checkboxColumn: true,
+		});
+	},
+
+	onload(report) {
+		// Material Request Button - Using exact Sales Order pattern
+		report.page.add_inner_button(__("Material Request"), () => {
+			this.make_material_request(report);
+		}, __("Create"));
+
+		// Purchase Order Button - Using exact Sales Order pattern  
+		report.page.add_inner_button(__("Purchase Order"), () => {
+			this.make_purchase_order(report);
+		}, __("Create"));
+
+		// Set Create as primary button group
+		report.page.set_inner_btn_group_as_primary(__("Create"));
+	},
+
+	make_material_request(report) {
+		const indexes = frappe.query_report.datatable.rowmanager.getCheckedRows();
+		const selected_rows = indexes.map((i) => frappe.query_report.data[i]);
+
+		if (!selected_rows.length) {
+			frappe.throw({
+				message: __("Please select rows to create Material Request"),
+				title: __("No Items Selected"),
+				indicator: "blue",
+			});
+		}
+
+		// Using the exact Sales Order pattern for creating Material Request
+		frappe.new_doc("Material Request", {
+			material_request_type: "Purchase",
+			items: selected_rows.map((row) => ({
+				item_code: row.item_code,
+				warehouse: row.warehouse,
+				qty: Math.abs(row.projected_qty),
+				schedule_date: frappe.datetime.add_days(frappe.datetime.get_today(), 7),
+			})),
+		});
+	},
+
+	make_purchase_order(report) {
+		const indexes = frappe.query_report.datatable.rowmanager.getCheckedRows();
+		const selected_rows = indexes.map((i) => frappe.query_report.data[i]);
+
+		if (!selected_rows.length) {
+			frappe.throw({
+				message: __("Please select rows to create Purchase Order"),
+				title: __("No Items Selected"),
+				indicator: "blue",
+			});
+		}
+
+		// Using exact Sales Order dialog pattern
+		var dialog = new frappe.ui.Dialog({
+			title: __("Select Items"),
+			size: "large",
+			fields: [
+				{
+					fieldtype: "Check",
+					label: __("Against Default Supplier"),
+					fieldname: "against_default_supplier",
+					default: 0,
+				},
+				{
+					fieldname: "items_for_po",
+					fieldtype: "Table",
+					label: __("Select Items"),
+					fields: [
+						{
+							fieldtype: "Data",
+							fieldname: "item_code",
+							label: __("Item"),
+							read_only: 1,
+							in_list_view: 1,
+						},
+						{
+							fieldtype: "Data",
+							fieldname: "warehouse",
+							label: __("Warehouse"),
+							read_only: 1,
+							in_list_view: 1,
+						},
+						{
+							fieldtype: "Float",
+							fieldname: "pending_qty",
+							label: __("Required Qty"),
+							read_only: 1,
+							in_list_view: 1,
+						},
+						{
+							fieldtype: "Data",
+							fieldname: "projected_qty",
+							label: __("Projected Qty"),
+							read_only: 1,
+							in_list_view: 1,
+						},
+					],
+					data: selected_rows.map((row) => ({
+						item_code: row.item_code,
+						warehouse: row.warehouse,
+						pending_qty: Math.abs(row.projected_qty),
+						projected_qty: row.projected_qty,
+					})),
+					cannot_add_rows: true,
+					cannot_delete_rows: true,
+				},
+			],
+			primary_action_label: __("Create Purchase Order"),
+			primary_action(args) {
+				if (!args) return;
+
+				let selected_items = dialog.fields_dict.items_for_po.grid.get_selected_children();
+				if (selected_items.length == 0) {
+					frappe.throw({
+						message: __("Please select Items from the Table"),
+						title: __("Items Required"),
+						indicator: "blue",
+					});
+				}
+
+				dialog.hide();
+
+				// Create Purchase Order directly (simplified from Sales Order pattern)
+				frappe.new_doc("Purchase Order", {
+					items: selected_items.map((item) => ({
+						item_code: item.item_code,
+						warehouse: item.warehouse,
+						qty: item.pending_qty,
+						schedule_date: frappe.datetime.add_days(frappe.datetime.get_today(), 7),
+					})),
+				});
+			},
+		});
+
+		dialog.show();
+	},
 };
